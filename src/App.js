a3d175c98228e5ff712e10213265c3283904c8c3
Emrah <ozyazgane@hotmail.com>
2020-02-05 09:28:01 +0300

    Initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>This is App</p>
+    </div>
+  );
+}
+
+export default App;

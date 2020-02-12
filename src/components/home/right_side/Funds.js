7ed752aa637c73d16a0f6e1042061db417f04353
Emrah <ozyazgane@hotmail.com>
2020-02-12 11:59:03 +0300

    Markets Page is almost done. Now we will create Funds Page

--- src/components/home/right_side/Funds.js
+++ src/components/home/right_side/Funds.js
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { Col, Table } from "react-bootstrap";
 import { FundItem } from './sub/FundItem';
 
 
 export const Funds = () => {
   return (
-    <div
-      id='funds-section'
-      className='  w-100'
-      style={{
-
-      }}
-    >
+    <Fragment>    
       <span className='funds-header'>
         <p className='d-block text-center text-light'>Funds</p>
       </span>
-      <Table striped bordered hover variant='dark' size='sm'>
-        <tbody>
-          <FundItem />
-          <FundItem />
-          <FundItem />
-          <FundItem />
-          <FundItem />
-        </tbody>
-      </Table>
+      <div
+        id='funds-section'
+        className='  w-100 bg-dark'
+        style={{
+          borderBottomLeftRadius:'1rem',
+          borderBottomRightRadius:'1rem'
+        }}
+      >
+        <Table striped bordered hover variant='dark' size='sm'>
+          <tbody className='scrollbar'>
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+            <FundItem />
+          </tbody>
+        </Table>
 
-    </div>
+      </div>
+    </Fragment>
 
   );
 }

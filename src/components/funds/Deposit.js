f32d28083eb29a12fab528017e9e91cc28b9e497
Emrah <ozyazgane@hotmail.com>
2020-02-18 14:32:04 +0300

    Funds Page / Deposit Tab is complete

--- src/components/funds/Deposit.js
+++ src/components/funds/Deposit.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Form, Table } from "react-bootstrap";
-import AssetsItem from './sub/AssetsItem';
+import DepositHistoryItem from './sub/DepositHistoryItem';
 
 
 const Deposit = () => {
   return (
     <div
       id='deposit-tab'
-      className='bg-warning'
+      // className='bg-'
+      style={{
+        backgroundColor:'#777'
+      }}
     >
       {/* Deposit Assets */}
       <div
         id='deposit-assets'
-        className='bg-primary d-block'
+        className='bg-dark d-block'
       >
         {/* Deposit Assets Header */}
         <div
           id='deposit-assets-header'
           >
-          <span className='header-text'>Deposit Assets</span> 
+          <span className='header-text text-light'>Deposit Assets</span> 
         </div>
           {/* End of  Deposit Assets Header */}
         
@@ -31,7 +34,7 @@ const Deposit = () => {
 
             <Form.Control
                as="select"
-               className='bg-dark text-white'
+              className='bg-dark text-light'
                style={{
                 //  color:'blue',
                 //  backgroundColor:'orange'
@@ -47,22 +50,22 @@ const Deposit = () => {
               className="d-flex flex-column"
             >
               <div className="d-flex flex-row justify-content-between">
-                <span>Total Balance</span>
-                <span>1.50000000 BTC</span>
+                <span className='text-light'>Total Balance</span>
+                <span className='text-light'>1.50000000 BTC</span>
               </div>
               <div className="d-flex flex-row justify-content-between">
-                <span>In Order</span>
-                <span>0.50000000 BTC</span>
+                <span className='text-light'>In Order</span>
+                <span className='text-light'>0.50000000 BTC</span>
               </div>
               <div className="d-flex flex-row justify-content-between">
-                <span>Available Balance</span>
-                <span>1.50000000 BTC</span>
+                <span className='text-light'>Available Balance</span>
+                <span className='text-light'>1.50000000 BTC</span>
               </div>
             </div>
 
             <div
               id='deposit-explanation'
-              className='text-sm-2'
+              className='text-sm-2 text-light'
             >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore illo reprehenderit voluptate, vitae tempore ullam rerum quis, sit eum quam unde vel dolor. Recusandae laboriosam tempore doloremque cumque esse. Ad dolores temporibus ea corporis tenetur vero harum necessitatibus saepe nam natus. Deleniti laudantium beatae magnam voluptas harum ut officiis at sit expedita minima! 
             </div>
@@ -73,12 +76,13 @@ const Deposit = () => {
           <div className="col sm-6">
             <p
               id='for-future-exp'
+              className='text-light'
             >
               BU ALANA DEPOSIT YAPACAĞI ADRES BAZI BUTONLAR VS GELECEK FAKAT BİZİM SİSTEMİMİZ İÇİN ŞU ANDA BU ALANA NELER GELECEĞİ BELİRSİZ.
             </p>
             <div
               id='for-future-bottom-exp'
-              className='text-sm-2'
+              className='text-sm-2  text-light'
             >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore illo reprehenderit voluptate, vitae tempore ullam rerum quis, sit eum quam unde vel dolor. Recusandae laboriosam tempore doloremque cumque esse. Ad dolores temporibus ea corporis tenetur vero harum necessitatibus saepe nam natus. Deleniti laudantium beatae magnam voluptas harum ut officiis at sit expedita minima!
             </div>
@@ -94,15 +98,63 @@ const Deposit = () => {
 
       <div
         id='deposit-history'
-        className='bg-primary d-block'
+        className='bg-dark d-block'
       >
         {/* Deposit History Header */}
         <div
           id='deposit-history-header'
         >
-          <span className='header-text'>Deposit History</span>
+          <span className='header-text  text-light'>Deposit History</span>
         </div>
         {/* End of  Deposit History Header */}
+
+        <Table
+          size='sm'
+          striped
+          bordered
+          // hover
+          variant='dark'
+          className=''
+        // style={{height:'30vh'}}
+        >
+          <thead>
+            <tr
+              className=''
+            >
+              <th
+                className='col-others'
+              >Date</th>
+              <th className='col-others'>Status</th>
+              <th
+                className='col-assets'
+              >Assets</th>
+              <th className='col-others'>Ticker</th>
+              <th className='col-others'>Amount</th>
+              <th
+                className='col-from'
+              >From</th>
+            </tr>
+          </thead>
+          <tbody
+            className='scrollbar'
+          // style={{ height: "25vh !important" }}
+          >
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+            <DepositHistoryItem />
+          </tbody>
+
+          {/*  */}
+        </Table>
 
         
 
--- src/components/funds/sub/DepositHistoryItem.js
+++ src/components/funds/sub/DepositHistoryItem.js
@@ -2,10 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const DepositHistoryItem = () => {
+  const textSize = ' text-sm-2 '
+  // const textColor = ' bg '
   return (
-    <div>
-      
-    </div>
+    <tr>
+      <td className={`${textSize} col-others`}>0.0000</td>
+      <td className={`${textSize} col-others`}>Complete</td>
+      <td className={`${textSize} col-assets`} >Basic Attention</td>
+      <td className={`${textSize} col-others`}>USDT</td>
+      <td className={`${textSize} col-others`}>0.0000</td>
+      <td className={`${textSize} col-from`}>0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae</td>
+    </tr>
   )
 }
 

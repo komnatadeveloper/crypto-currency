3f9ee1b5617f90769fcf23080e39de78d974e6b3
Emrah <ozyazgane@hotmail.com>
2020-02-19 22:28:16 +0300

    Some CSS Workout for Funds Page, Search Input, Checkbox, and Date Inputs has been done

--- src/components/funds/TradeHistory.js
+++ src/components/funds/TradeHistory.js
@@ -1,44 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import { Form, Table } from "react-bootstrap";
 import TradeHistoryItem from './sub/TradeHistoryItem';
+import 'react-datepicker/dist/react-datepicker.css'
+import DatePicker from 'react-datepicker';
 
 
 const TradeHistory = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
   return (
-    <div
-      id='trade-history-tab'
-      className='bg-dark'
-    >
+    <div id='trade-history-tab' className='bg-dark'>
       {/* Search And Checkbox Section */}
-      <div
-        id='trade-history-tab-bar'
-        className='bg-primary'
-      >
-        <Form
-          className="form-inline m-0"
-          id="search-assets"
-        >
-          <span
-            className="fa fa-search text-dark icon-right"
-          ></span>
-          <Form.Control
-            type="search"
-            placeholder="Search Assets"
-          />
-          {/* <Form.Control
-            type="date"
-            // placeholder="Search Assets"
-          />
-
-          <Form.Check
-            className='text-black'
-            id="hide-zero"
-            type="checkbox"
-            label="Hide 0 Balances"
-          ></Form.Check> */}
-
+      <div id='trade-history-tab-bar' className='bg-primary'>
+        <Form className='form-inline m-0' id='search-assets'>
+          <span className='fa fa-search text-dark icon-right'></span>
+          <Form.Control type='search' placeholder='Search Assets' />
         </Form>
+        
+        {/* <DatePicker
+          selected={ selectedDate}
+          onChange= { date => setSelectedDate(date)}
+          popperPlacement= 'bottom-end'
+          // showTimeSelect
+          // dateFormat='Pp'
+          style={{
+            // position:'relative',
+            // top:'0',
+            // left: '15rem'
+          }}
+          className=''
+        /> */}
+
+        {/* <Form.Check
+          className='text-black'
+          id='hide-zero1'
+          type='date'
+          label='Hide 0 Balances'
+        ></Form.Check> */}
+        <input
+          type='date'
+          id='date-select'
+          style={{}}
+        />
 
       </div>
       {/* End of Search And Checkbox Section */}
@@ -51,12 +55,10 @@ const TradeHistory = () => {
         // hover
         variant='dark'
         className=''
-      // style={{height:'30vh'}}
+        // style={{height:'30vh'}}
       >
         <thead>
-          <tr
-            className=''
-          >            
+          <tr className=''>
             <th className='text-center'>Date</th>
             <th className='text-center'>Market</th>
             <th className='text-center'>Asset</th>
@@ -69,7 +71,7 @@ const TradeHistory = () => {
         </thead>
         <tbody
           className='scrollbar'
-        // style={{ height: "25vh !important" }}
+          // style={{ height: "25vh !important" }}
         >
           <TradeHistoryItem />
           <TradeHistoryItem />
@@ -106,12 +108,11 @@ const TradeHistory = () => {
           <TradeHistoryItem />
           {/* <TradeHistoryItem />
           <TradeHistoryItem /> */}
-
         </tbody>
       </Table>
       {/* End of  Table */}
     </div>
-  )
+  );
 }
 
 TradeHistory.propTypes = {
